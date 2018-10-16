eb7a772aa02a81db7baf9f62e5fd8c584c860fb5
Danil Kazimirov <danil.kazim.99@gmail.com>
2018-10-16 12:42:33 +0300

    feat(pg-monitor): add loging via debug

--- src/services/pg-monitor.ts
+++ src/services/pg-monitor.ts
@@ -2,22 +2,23 @@ import * as pgMonitor from 'pg-monitor';
 import { IOptions } from 'pg-promise';
 import * as config from '../config';
 import { getLogger } from '../helpers/logger';
+import * as Debug from 'debug';
+
+const debug = Debug('app:pg:monitor');
 
 pgMonitor.setTheme('matrix'); // changing the default theme;
 const log = getLogger({ name: 'pg' });
 
 pgMonitor.setLog((msg, info) => {
+  info.display = false; // display nothing;
   if (info.event === 'error') {
     log.error('%O', msg);
   }
-  if (config.env.IS_PRODUCTION) {
-    // If it is not a DEV environment:
-    info.display = false; // display nothing;
-  }
+  debug(msg);
 });
 // Monitor initialization function;
 export function init(options: IOptions<any>) {
-  if (config.env.IS_PRODUCTION) {
+  if (!config.env.IS_PRODUCTION) {
       // In a DEV environment, we attach to all supported events:
     pgMonitor.attach(options);
   } else {
