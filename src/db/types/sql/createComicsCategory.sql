3f2e1fa751c6c6f35957f1eeedf45d0cc3f4e6ca
Danil Kazimirov <danil.kazim.99@gmail.com>
2018-10-29 22:14:27 +0200

    fix creation comics_category

--- src/db/types/sql/createComicsCategory.sql
+++ src/db/types/sql/createComicsCategory.sql
@@ -1,10 +1,15 @@
-CREATE TYPE comics_category AS ENUM
-(
-    'manga',
-    'science fiction',
-    'fantasy',
-    'action',
-    'horror',
-    'romance',
-    'adult'
+DO $$
+BEGIN
+IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'comics_category') THEN
+create type comics_category AS ENUM (
+'manga',
+'science fiction',
+'fantasy',
+'action',
+'horror',
+'romance',
+'adult'
 );
+END IF;
+END
+$$;
